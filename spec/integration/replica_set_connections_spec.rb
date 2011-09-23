c9d8f0befa2924a28f12fdadf173d2a769870c20
Markus Schirp <mbj@seonic.net>
2011-09-23 18:36:03 +0200

    Fix for repo name in repl set connection

--- spec/integration/replica_set_connections_spec.rb
+++ spec/integration/replica_set_connections_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe 'replica set connections' do
   with_running_replica_set do
+    let(:repo_name) { :replica_set_connection }
+
     it 'should connect the replica set' do
       DataMapper.setup(
-        :replica_set_connection,
+        repo_name,
         :adapter => 'mongo',
         :seeds => replica_set
       )
-      connection = DataMapper.repository(:default).adapter.send(:connection)
+      connection = DataMapper.repository(repo_name).adapter.send(:connection)
       connection.should be_kind_of(Mongo::ReplSetConnection)
     end
 
