610b3d7c924a3c23cb095d7838e3e90048f50aff
Markus Schirp <mbj@seonic.net>
2011-09-23 13:39:13 +0200

    Add Database tests under travis #2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,7 +44,7 @@ end
 
 module ConnectionManagement
   def with_connection
-    if ENV['MONGO_URL'] or ENV['TRAVIS_CI']
+    if ENV['MONGO_URL'] or ENV['TRAVIS']
       before :all do
         setup_connection
       end
