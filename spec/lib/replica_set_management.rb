375a2bad0a3de67ab6f2e02eaeee91522b81566e
Markus Schirp <mbj@seonic.net>
2011-10-05 18:26:03 +0200

    added a replica set spec guard for ruby

--- spec/lib/replica_set_management.rb
+++ spec/lib/replica_set_management.rb
@@ -1,14 +1,22 @@
 module DataMapper::Mongo::Spec
   module ReplicaSetManagement
     def with_running_replica_set
+      if RUBY_PLATFORM == 'java'
+        describe 'replica set specs' do
+          specify 'are not supported' do
+            pending 'under jruby'
+          end
+        end
+        return
+      end
       before :all do
         @replica_set = start_replica_set
       end
-   
+      
       let(:replica_set) { @replica_set }
-   
+      
       yield
-   
+      
       after :all do
         teardown_replica_set
       end
